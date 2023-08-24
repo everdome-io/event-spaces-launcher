4bcafe721aa5f5f126f403ed170197bdd69290ba
Pawel Mizwa <pawel.mizwa@pragmaticcoders.com>
2023-08-24 11:04:23 +0200

    initial build

--- assets/okx/mcohilncbfahbmgdjkbpemcciiolgcge/2.40.0_0/scripts/ui/chunk-GGV226OL.js
+++
@@ -1,5 +0,0 @@
-import{i as e,k as f}from"./chunk-YC2Z7II3.js";e();f();
-
-window.inOKXExtension = true;
-window.ASSETS_BUILD_TYPE = "publish";
-
--- assets/okx/mcohilncbfahbmgdjkbpemcciiolgcge/2.40.0_0/scripts/ui/chunk-LRBTGOAF.js
+++
@@ -1,5 +0,0 @@
-import{i as o,k as t}from"./chunk-YC2Z7II3.js";o();t();
-
-window.inOKXExtension = true;
-window.ASSETS_BUILD_TYPE = "publish";
-
